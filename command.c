e038e2394f48a3a15093eaf07d6a7edbed00fd68
greeshmasamhitha <greeshmasreenivasulu@gmail.com>
2023-06-24 12:15:03 +0530

    this is my committed files

---
+++ command.c
@@ -0,0 +1,17 @@
+git clone(to clone the remote repositary into local machine
+cd 
+ll(to se files in long list)
+ls(to se files in short list)
+ls -a(to see the long list of files and directories including hidden files)
+git status(to see the status of the files)
+git add/stage(to add files in staging or index)
+git commit -m "message" filename(to send particular file)
+git commit -m "message" ( to send all files)
+git add/stage *(to add all the files in staging or index)
+git stage/add filename filename (to add 2 files at a time)
+git branch(to know whether the file is in main branch or not)
+git reset filename(to change the directory from stage or commited to working directory)
+git-global information track
+git meant for source code management(SCM)
+
+
